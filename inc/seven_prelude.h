5eb6a205a25a083c5ebc2685993b120cca4267bb
Luna Catkins <lunarlambda@gmail.com>
2021-10-07 13:35:48 +0200

    add prelude header

---
+++ inc/seven_prelude.h
@@ -0,0 +1,18 @@
+#ifndef _LIBSEVEN_PRELUDE_H
+#define _LIBSEVEN_PRELUDE_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <seven_base.h>
+#include <seven_input.h>
+#include <seven_irq.h>
+#include <seven_memory.h>
+#include <seven_video.h>
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* !_LIBSEVEN_PRELUDE_H */
