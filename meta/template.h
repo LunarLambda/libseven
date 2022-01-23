a02817ea96863b46632b68ee8ba3a5b006b877bd
Luna Catkins <lunarlambda@gmail.com>
2022-01-23 14:50:11 +0100

    add template files for source code

---
+++ meta/template.h
@@ -0,0 +1,16 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+#ifndef _LIBSEVEN_TEMPLATE_H
+#define _LIBSEVEN_TEMPLATE_H
+
+#include <seven/base.h>
+
+_LIBSEVEN_EXTERN_C
+
+_LIBSEVEN_EXTERN_C_END
+
+#endif /* !_LIBSEVEN_TEMPLATE_H */
---
+++ meta/template.s
@@ -0,0 +1,12 @@
+@
+@ This Source Code Form is subject to the terms of the Mozilla Public
+@ License, v. 2.0. If a copy of the MPL was not distributed with this
+@ file, You can obtain one at https://mozilla.org/MPL/2.0/.
+@
+
+.syntax         unified
+.cpu            arm7tdmi
+
+.include        "macros.s"
+
+@ vim: ft=armv4 et sta sw=4 sts=8
