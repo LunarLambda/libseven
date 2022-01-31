01669a7c89cf3122d0346a0d53194a40f1f148fb
Luna Catkins <lunarlambda@gmail.com>
2022-01-31 11:47:45 +0100

    meta: add assembly includes

---
+++ include/seven/asm/base.s
@@ -0,0 +1,12 @@
+@
+@ This Source Code Form is subject to the terms of the Mozilla Public
+@ License, v. 2.0. If a copy of the MPL was not distributed with this
+@ file, You can obtain one at https://mozilla.org/MPL/2.0/.
+@
+
+.ifndef         _LIBSEVEN_ASM_BASE_S
+.equiv          _LIBSEVEN_ASM_BASE_S, 1
+
+.endif          @ !_LIBSEVEN_ASM_BASE_S
+
+@ vim: ft=armv4 et sta sw=4 sts=8
---
+++ meta/include.s
@@ -0,0 +1,14 @@
+@
+@ This Source Code Form is subject to the terms of the Mozilla Public
+@ License, v. 2.0. If a copy of the MPL was not distributed with this
+@ file, You can obtain one at https://mozilla.org/MPL/2.0/.
+@
+
+.ifndef         _LIBSEVEN_ASM_TEMPLATE_S
+.equiv          _LIBSEVEN_ASM_TEMPLATE_S, 1
+
+.include        "seven/asm/base.s"
+
+.endif          @ !_LIBSEVEN_ASM_TEMPLATE_S
+
+@ vim: ft=armv4 et sta sw=4 sts=8
