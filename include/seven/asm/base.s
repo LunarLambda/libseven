a494be71b4100708085f24c1024ff78769935c22
Luna Catkins <lunarlambda@gmail.com>
2022-02-01 18:58:39 +0100

    include/asm: implement base macros

--- include/seven/asm/base.s
+++ include/seven/asm/base.s
@@ -4,9 +4,37 @@
 @ file, You can obtain one at https://mozilla.org/MPL/2.0/.
 @
 
-.ifndef         _LIBSEVEN_ASM_BASE_S
-.equiv          _LIBSEVEN_ASM_BASE_S, 1
+.ifndef _LIBSEVEN_ASM_BASE_S
+.equiv  _LIBSEVEN_ASM_BASE_S, 1
 
-.endif          @ !_LIBSEVEN_ASM_BASE_S
+.equiv REG_BASE, 0x04000000
+
+.macro CONST id:req, v:req
+    .equiv \id, \v
+.endm
+
+.macro REG id:req addr:req
+    CONST REG_\id, \addr
+    CONST OFF_\id, (\addr) - REG_BASE
+.endm
+
+.macro BIT id:req n:req
+    CONST \id, 1 << (\n)
+.endm
+
+.macro BITS id:req n:req
+    CONST \id, (1 << (\n)) - 1
+.endm
+
+.macro BITFIELD id:req sid:req value:req
+    CONST \id\()_\sid, ((\value) & ((1 << BF_\id\()_LENGTH) - 1)) << BF_\id\()_OFFSET
+.endm
+
+.macro EXTFN id:req
+    .extern \id
+    .type   \id STT_FUNC
+.endm
+
+.endif @ !_LIBSEVEN_ASM_BASE_S
 
 @ vim: ft=armv4 et sta sw=4 sts=8
