0be8adc10ab03d500777044cfc5a346ee6c87a1b
Luna Catkins <lunarlambda@gmail.com>
2022-02-02 17:16:58 +0100

    blend: cleanup bitfields, change prefix

--- include/seven/video/blend.h
+++ include/seven/video/blend.h
@@ -12,34 +12,35 @@
 _LIBSEVEN_EXTERN_C
 
 #define REG_BLDCNT      REG16(0x04000050)
-// TODO: BLDALPHA defines
 #define REG_BLDALPHA    REG16(0x04000052)
 #define REG_BLDCOEFF    REG16(0x04000054)
 
-#define BF_BLD_MODE_OFFSET 6
-#define BF_BLD_MODE_LENGTH 2
+#define BF_BLEND_MODE_OFFSET 6
+#define BF_BLEND_MODE_LENGTH 2
+
+#define BLEND_MODE(n) BITFIELD(BLEND_MODE, (n))
 
 enum BlendControl
 {
-    BLD_TARGET_BG0      = BIT(0),
-    BLD_TARGET_BG1      = BIT(1),
-    BLD_TARGET_BG2      = BIT(2),
-    BLD_TARGET_BG3      = BIT(3),
-    BLD_TARGET_OBJ      = BIT(4),
-    BLD_TARGET_BD       = BIT(5),
+    BLEND_TARGET_BG0      = BIT(0),
+    BLEND_TARGET_BG1      = BIT(1),
+    BLEND_TARGET_BG2      = BIT(2),
+    BLEND_TARGET_BG3      = BIT(3),
+    BLEND_TARGET_OBJ      = BIT(4),
+    BLEND_TARGET_BD       = BIT(5),
 
-    BLD_MODE_NONE       = BITFIELD(BLD_MODE, 0),
-    BLD_MODE_ALPHA      = BITFIELD(BLD_MODE, 1),
-    BLD_MODE_WHITE      = BITFIELD(BLD_MODE, 2),
-    BLD_MODE_BLACK      = BITFIELD(BLD_MODE, 3),
+    BLEND_MODE_NONE       = BLEND_MODE(0),
+    BLEND_MODE_ALPHA      = BLEND_MODE(1),
+    BLEND_MODE_WHITE      = BLEND_MODE(2),
+    BLEND_MODE_BLACK      = BLEND_MODE(3),
 
-    // Only used with BLD_MODE_ALPHA
-    BLD_TARGET2_BG0      = BIT(8),
-    BLD_TARGET2_BG1      = BIT(9),
-    BLD_TARGET2_BG2      = BIT(10),
-    BLD_TARGET2_BG3      = BIT(11),
-    BLD_TARGET2_OBJ      = BIT(12),
-    BLD_TARGET2_BD       = BIT(13),
+    // Only used with BLEND_MODE_ALPHA
+    BLEND_TARGET2_BG0     = BIT(8),
+    BLEND_TARGET2_BG1     = BIT(9),
+    BLEND_TARGET2_BG2     = BIT(10),
+    BLEND_TARGET2_BG3     = BIT(11),
+    BLEND_TARGET2_OBJ     = BIT(12),
+    BLEND_TARGET2_BD      = BIT(13),
 };
 
 _LIBSEVEN_EXTERN_C_END
