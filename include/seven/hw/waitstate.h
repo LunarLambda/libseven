5ec8b7793c8669f8d2c697725169797a2e20be7e
Luna Catkins <lunarlambda@gmail.com>
2022-02-02 17:19:24 +0100

    waitstate: add bitfield constructors

--- include/seven/hw/waitstate.h
+++ include/seven/hw/waitstate.h
@@ -16,26 +16,32 @@ _LIBSEVEN_EXTERN_C
 #define BF_WAIT_SRAM_OFFSET     0
 #define BF_WAIT_SRAM_LENGTH     2
 
+#define WAIT_SRAM(n) BITFIELD(WAIT_SRAM, (n))
+
 #define BF_WAIT_ROM_N_OFFSET    2
 #define BF_WAIT_ROM_N_LENGTH    2
+
+#define WAIT_ROM_N(n) BITFIELD(WAIT_ROM_N, (n))
 
 #define BF_WAIT_ROM_S_OFFSET    4
 #define BF_WAIT_ROM_S_LENGTH    1
 
+#define WAIT_ROM_S(n) BITFIELD(WAIT_ROM_S, (n))
+
 enum Waitstate
 {
-    WAIT_SRAM_4          = BITFIELD(WAIT_SRAM, 0),
-    WAIT_SRAM_3          = BITFIELD(WAIT_SRAM, 1),
-    WAIT_SRAM_2          = BITFIELD(WAIT_SRAM, 2),
-    WAIT_SRAM_8          = BITFIELD(WAIT_SRAM, 3),
+    WAIT_SRAM_4          = WAIT_SRAM(0),
+    WAIT_SRAM_3          = WAIT_SRAM(1),
+    WAIT_SRAM_2          = WAIT_SRAM(2),
+    WAIT_SRAM_8          = WAIT_SRAM(3),
 
-    WAIT_ROM_N_4         = BITFIELD(WAIT_ROM_N, 0),
-    WAIT_ROM_N_3         = BITFIELD(WAIT_ROM_N, 1),
-    WAIT_ROM_N_2         = BITFIELD(WAIT_ROM_N, 2),
-    WAIT_ROM_N_8         = BITFIELD(WAIT_ROM_N, 3),
+    WAIT_ROM_N_4         = WAIT_ROM_N(0),
+    WAIT_ROM_N_3         = WAIT_ROM_N(1),
+    WAIT_ROM_N_2         = WAIT_ROM_N(2),
+    WAIT_ROM_N_8         = WAIT_ROM_N(3),
 
-    WAIT_ROM_S_2         = BITFIELD(WAIT_ROM_S, 0),
-    WAIT_ROM_S_1         = BITFIELD(WAIT_ROM_S, 1),
+    WAIT_ROM_S_2         = WAIT_ROM_S(0),
+    WAIT_ROM_S_1         = WAIT_ROM_S(1),
 
     WAIT_PREFETCH_ENABLE = BIT(14),
 };
