f0c65e335a4d7ff823a8cf83894a3e6dc276a65a
Luna Catkins <lunarlambda@gmail.com>
2022-02-02 17:15:45 +0100

    video: bg_tiled->bg_regular, add defines

---
+++ include/seven/video/bg_regular.h
@@ -0,0 +1,32 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+#ifndef _LIBSEVEN_VIDEO_BG_REGULAR_H
+#define _LIBSEVEN_VIDEO_BG_REGULAR_H
+
+#include <seven/base.h>
+
+_LIBSEVEN_EXTERN_C
+
+#define BF_TILE_NUMBER_OFFSET 0
+#define BF_TILE_NUMBER_LENGTH 10
+
+#define TILE_NUMBER(n) BITFIELD(TILE_NUMBER, (n))
+
+#define BG_TILE_PALETTE_OFFSET 12
+#define BG_TILE_PALETTE_LENGTH 4
+
+#define TILE_PALETTE(n) BITFIELD(TILE_PALETTE, (n))
+
+enum Tile
+{
+    TILE_FLIP_H = BIT(10),
+    TILE_FLIP_V = BIT(11),
+}
+
+_LIBSEVEN_EXTERN_C_END
+
+#endif /* !_LIBSEVEN_VIDEO_BG_REGULAR_H */
