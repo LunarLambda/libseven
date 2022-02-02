7c4ce017098966b2bd393451107ca6de913fd4ac
Luna Catkins <lunarlambda@gmail.com>
2022-02-02 17:17:33 +0100

    color: add bitfield constructors

--- include/seven/video/color.h
+++ include/seven/video/color.h
@@ -13,13 +13,21 @@ _LIBSEVEN_EXTERN_C
 
 #define BF_COLOR_R_OFFSET 0
 #define BF_COLOR_R_LENGTH 5
+
+#define COLOR_R(n) BITFIELD(COLOR_R, (n))
+
 #define BF_COLOR_G_OFFSET 5
 #define BF_COLOR_G_LENGTH 5
+
+#define COLOR_G(n) BITFIELD(COLOR_G, (n))
+
 #define BF_COLOR_B_OFFSET 10
 #define BF_COLOR_B_LENGTH 5
 
+#define COLOR_B(n) BITFIELD(COLOR_B, (n))
+
 #define RGB5(r, g, b) \
-    ((u16)(BITFIELD(COLOR_R, (r)) | BITFIELD(COLOR_G, (g)) | BITFIELD(COLOR_B, (b))))
+    ((u16)(COLOR_R((r)) | COLOR_G((g)) | COLOR_B((b))))
 
 #define RGB8(r, g, b) \
     RGB5((r) >> 3, (g) >> 3, (b) >> 3)
