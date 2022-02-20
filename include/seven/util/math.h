a555ea3bc0e5b07122dd3a06eae41a51fb5296cb
Luna Catkins <lunarlambda@gmail.com>
2022-02-20 19:35:57 +0100

    add util/math header stub

---
+++ include/seven/util/math.h
@@ -0,0 +1,18 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+#ifndef _LIBSEVEN_UTIL_MATH_H
+#define _LIBSEVEN_UTIL_MATH_H
+
+#include <seven/base.h>
+
+_LIBSEVEN_EXTERN_C
+
+#error "seven/util/math.h is a stub header"
+
+_LIBSEVEN_EXTERN_C_END
+
+#endif /* !_LIBSEVEN_UTIL_MATH_H */
