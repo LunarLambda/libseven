d965210019850e5454425a2325bc4f046050664d
Luna Catkins <lunarlambda@gmail.com>
2022-02-24 21:07:21 +0100

    profile: code cleanup

--- src/util/profile.s
+++ src/util/profile.s
@@ -15,37 +15,28 @@
 fn profileRun arm
     push        {r4, r5, r6, lr}
 
-    ldr         r4, =REG_TM2VAL
-    mov         r5, #0
-    mov         r6, r1
+    mov         r4, r1
 
-    @ REG_TM2VAL = 0
-    @ REG_TM3VAL = 0
-    strh        r5, [r4]
-    strh        r5, [r4, #4]
-    @ REG_TM3CNT
-    mov         r2, TIMER_FREQ_CASCADE
-    strh        r2, [r4, #6]
-    @ REG_TM2CNT
-    mov         r2, TIMER_ENABLE
-    strh        r2, [r4, #2]
+    @ Setup timers
+    ldr         r5, =REG_TM2VAL
+    mov         r6, #TIMER_FREQ_CASCADE << 16
+    mov         r1, #TIMER_ENABLE << 16
+    stmia       r5, {r1, r6}
 
-    @ Call fntion
+    @ Call function
     mov         lr, pc
     bx          r0
 
     @ Stop timer
-    strh        r5, [r4, #2]
+    strh        r6, [r5, #2]
 
     @ Store return value?
-    movs        r6, r6
-    beq         .Lskip
-    str         r0, [r6]
-.Lskip:
+    cmp         r4, #0
+    strne       r0, [r4]
 
     @ Read timers
-    ldrh        r0, [r4]
-    ldrh        r1, [r4, #4]
+    ldrh        r0, [r5]
+    ldrh        r1, [r5, #4]
     orr         r0, r0, r1, lsl #16
 
     pop         {r4, r5, r6, lr}
