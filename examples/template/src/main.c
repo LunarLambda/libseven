e114cc48e530677d6fd02ef2dcb6a6bd10329a2a
Luna Catkins <lunarlambda@gmail.com>
2022-02-26 15:22:54 +0100

    template: simplify

--- examples/template/src/main.c
+++ examples/template/src/main.c
@@ -1,20 +1,13 @@
-#include <seven/prelude.h>
 #include <seven/video/prelude.h>
 #include <seven/video/bg_bitmap.h>
 
 int main(void)
 {
-    irqInitDefault();
-    irqEnableFull(IRQ_VBLANK);
-
-    lcdInitMode3();
+    REG_DISPCNT = VIDEO_MODE_BITMAP | VIDEO_BG2_ENABLE;
 
     MODE3_FRAME[80][120] = RGB5(31,  0,  0);
     MODE3_FRAME[80][136] = RGB5( 0, 31,  0);
     MODE3_FRAME[96][120] = RGB5( 0,  0, 31);
 
-    while (1)
-    {
-        svcVBlankIntrWait();
-    }
+    while (1) {}
 }
