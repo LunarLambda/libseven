f00c06af8dc12cb853392f2069bdf5432b8da0cc
n67094 <n67094@gmail.com>
2022-08-15 15:55:12 +0200

    renaming object attributes

--- include/seven/video/object.h
+++ include/seven/video/object.h
@@ -20,105 +20,105 @@ struct Object
     u16 prio;
 } ALIGN(4);
 
-#define BF_ATTR0_Y_OFFSET 0
-#define BF_ATTR0_Y_LENGTH 8
+#define BF_OBJ_Y_OFFSET 0
+#define BF_OBJ_Y_LENGTH 8
 
-#define ATTR0_Y(n) BITFIELD(ATTR0_Y, (n))
+#define OBJ_Y(n) BITFIELD(OBJ_Y, (n))
 
-#define BF_ATTR0_MODE_OFFSET 8
-#define BF_ATTR0_MODE_LENGTH 2
+#define BF_OBJ_MODE_OFFSET 8
+#define BF_OBJ_MODE_LENGTH 2
 
-#define ATTR0_MODE(n) BITFIELD(ATTR0_MODE, (n))
+#define OBJ_MODE(n) BITFIELD(OBJ_MODE, (n))
 
-#define BF_ATTR0_GFX_OFFSET 10
-#define BF_ATTR0_GFX_LENGTH 2
+#define BF_OBJ_GFX_OFFSET 10
+#define BF_OBJ_GFX_LENGTH 2
 
-#define ATTR0_GFX(n) BITFIELD(ATTR0_GFX, (n))
+#define OBJ_GFX(n) BITFIELD(OBJ_GFX, (n))
 
-#define BF_ATTR0_SHAPE_OFFSET 14
-#define BF_ATTR0_SHAPE_LENGTH 2
+#define BF_OBJ_SHAPE_OFFSET 14
+#define BF_OBJ_SHAPE_LENGTH 2
 
-#define ATTR0_SHAPE(n) BITFIELD(ATTR0_SHAPE, (n))
+#define OBJ_SHAPE(n) BITFIELD(OBJ_SHAPE, (n))
 
-enum Attribute0
+enum ObjectAttribute0
 {
-    ATTR0_MODE_REGULAR          = ATTR0_MODE(0),
-    ATTR0_MODE_AFFINE           = ATTR0_MODE(1),
-    ATTR0_MODE_HIDE             = ATTR0_MODE(2),
-    ATTR0_MODE_AFFINE_DOUBLE    = ATTR0_MODE(3),
+    OBJ_MODE_REGULAR          = OBJ_MODE(0),
+    OBJ_MODE_AFFINE           = OBJ_MODE(1),
+    OBJ_MODE_HIDE             = OBJ_MODE(2),
+    OBJ_MODE_AFFINE_DOUBLE    = OBJ_MODE(3),
 
-    ATTR0_GFX_REGULAR           = ATTR0_GFX(0),
-    ATTR0_GFX_BLEND             = ATTR0_GFX(1),
-    ATTR0_GFX_WINDOW            = ATTR0_GFX(2),
+    OBJ_GFX_REGULAR           = OBJ_GFX(0),
+    OBJ_GFX_BLEND             = OBJ_GFX(1),
+    OBJ_GFX_WINDOW            = OBJ_GFX(2),
 
-    ATTR0_MOSAIC_ENABLE         = BIT(12),
+    OBJ_MOSAIC_ENABLE         = BIT(12),
 
-    ATTR0_4BPP                  = BIT(13),
-    ATTR0_8BPP                  = !ATTR0_4BPP,
+    OBJ_4BPP                  = BIT(13),
+    OBJ_8BPP                  = !OBJ_4BPP,
 
-    ATTR0_SQUARE                = ATTR0_SHAPE(0),
-    ATTR0_HORIZONTAL            = ATTR0_SHAPE(1),
-    ATTR0_VERTICAL              = ATTR0_SHAPE(2),
+    OBJ_SQUARE                = OBJ_SHAPE(0),
+    OBJ_HORIZONTAL            = OBJ_SHAPE(1),
+    OBJ_VERTICAL              = OBJ_SHAPE(2),
 };
 
-#define BF_ATTR1_X_OFFSET 0
-#define BF_ATTR1_X_LENGTH 9
+#define BF_OBJ_X_OFFSET 0
+#define BF_OBJ_X_LENGTH 9
 
-#define ATTR1_X(n) BITFIELD(ATTR1_X, (n))
+#define OBJ_X(n) BITFIELD(OBJ_X, (n))
 
-#define BF_ATTR1_AFFINE_ID_OFFSET 9
-#define BF_ATTR1_AFFINE_ID_LENGTH 5
+#define BF_OBJ_AFFINE_ID_OFFSET 9
+#define BF_OBJ_AFFINE_ID_LENGTH 5
 
-#define ATTR1_AFFINE_ID(n) BITFIELD(ATTR1_AFFINE_ID, (n))
+#define OBJ_AFFINE_ID(n) BITFIELD(OBJ_AFFINE_ID, (n))
 
-#define BF_ATTR1_SIZE_OFFSET 14
-#define BF_ATTR1_SIZE_LENGTH 2
+#define BF_OBJ_SIZE_OFFSET 14
+#define BF_OBJ_SIZE_LENGTH 2
 
-#define ATTR1_SIZE(n) BITFIELD(ATTR1_SIZE, (n))
+#define OBJ_SIZE(n) BITFIELD(OBJ_SIZE, (n))
 
-enum Attribute1
+enum ObjectAttribute1
 {
-    ATTR1_HORIZONTAL_FLIP   = BIT(12),
-    ATTR1_VERTICAL_FLIP     = BIT(13),
+    OBJ_HORIZONTAL_FLIP   = BIT(12),
+    OBJ_VERTICAL_FLIP     = BIT(13),
 
     // Square
-    ATTR1_SIZE_8x8          = ATTR1_SIZE(0),
-    ATTR1_SIZE_16x16        = ATTR1_SIZE(1),
-    ATTR1_SIZE_32x32        = ATTR1_SIZE(2),
-    ATTR1_SIZE_64x64        = ATTR1_SIZE(3),
+    OBJ_SIZE_8x8          = OBJ_SIZE(0),
+    OBJ_SIZE_16x16        = OBJ_SIZE(1),
+    OBJ_SIZE_32x32        = OBJ_SIZE(2),
+    OBJ_SIZE_64x64        = OBJ_SIZE(3),
 
     // Hortizontal
-    ATTR1_SIZE_16x8         = ATTR1_SIZE(0),
-    ATTR1_SIZE_32x8         = ATTR1_SIZE(1),
-    ATTR1_SIZE_32x16        = ATTR1_SIZE(2),
-    ATTR1_SIZE_64x32        = ATTR1_SIZE(3),
+    OBJ_SIZE_16x8         = OBJ_SIZE(0),
+    OBJ_SIZE_32x8         = OBJ_SIZE(1),
+    OBJ_SIZE_32x16        = OBJ_SIZE(2),
+    OBJ_SIZE_64x32        = OBJ_SIZE(3),
 
     // Vertical
-    ATTR1_SIZE_8x16         = ATTR1_SIZE(0),
-    ATTR1_SIZE_8x32         = ATTR1_SIZE(1),
-    ATTR1_SIZE_16x32        = ATTR1_SIZE(2),
-    ATTR1_SIZE_32x64        = ATTR1_SIZE(3),
+    OBJ_SIZE_8x16         = OBJ_SIZE(0),
+    OBJ_SIZE_8x32         = OBJ_SIZE(1),
+    OBJ_SIZE_16x32        = OBJ_SIZE(2),
+    OBJ_SIZE_32x64        = OBJ_SIZE(3),
 };
 
-#define BF_ATTR2_TILE_ID_OFFSET 0
-#define BF_ATTR2_TILE_ID_LENGTH 9
+#define BF_OBJ_TILE_ID_OFFSET 0
+#define BF_OBJ_TILE_ID_LENGTH 9
 
-#define ATTR2_TILE_ID(n) BITFIELD(ATTR2_TILE_ID, (n))
+#define OBJ_TILE_ID(n) BITFIELD(OBJ_TILE_ID, (n))
 
-#define BF_ATTR2_PRIORITY_OFFSET 10
-#define BF_ATTR2_PRIORITY_LENGTH 2
+#define BF_OBJ_PRIORITY_OFFSET 10
+#define BF_OBJ_PRIORITY_LENGTH 2
 
-#define ATTR2_PRIORITY(n) BITFIELD(ATTR2_PRIORITY, (n))
+#define OBJ_PRIORITY(n) BITFIELD(OBJ_PRIORITY, (n))
 
-#define BF_ATTR2_PALETTE_BANK_OFFSET 12
-#define BF_ATTR2_PALETTE_BANK_LENGTH 4
+#define BF_OBJ_PALETTE_ID_OFFSET 12
+#define BF_OBJ_PALETTE_ID_LENGTH 4
 
-#define ATTR2_PALETTE_BANK(n) BITFIELD(ATTR2_PALETTE_BANK, (n))
+#define OBJ_PALETTE_ID(n) BITFIELD(OBJ_PALETTE_BANK, (n))
 
-enum Attribute2
+enum ObjectAttribute2
 {
-    ATTR2_PRIORITY_MIN = ATTR2_PRIORITY(3),
-    ATTR2_PRIORITY_MAX = ATTR2_PRIORITY(0),
+    OBJ_PRIORITY_MIN = OBJ_PRIORITY(3),
+    OBJ_PRIORITY_MAX = OBJ_PRIORITY(0),
 };
 
 _LIBSEVEN_EXTERN_C_END
